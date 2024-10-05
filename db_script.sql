131fdc16ff52c64ab4407874a4a3ca73eaaf0586
Sukruth Mothakapally <sukruth@pipeiq.ai>
2024-10-04 20:13:06 -0700

    remove all files as gitignore did not work

--- db_script.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE applied_companies (
-    id SERIAL PRIMARY KEY,
-    company_name VARCHAR(255) NOT NULL,
-    company_website VARCHAR(255),
-    job_position VARCHAR(255),
-    applied_date TIMESTAMP WITH TIME ZONE NOT NULL,
-    UNIQUE (company_name, job_position)
-);
