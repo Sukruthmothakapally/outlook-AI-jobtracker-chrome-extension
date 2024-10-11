70fabd8df7bac1a8db8968a8993b270cd5329a4e
Sukruth Mothakapally <sukruth@pipeiq.ai>
2024-10-10 17:39:53 -0700

    commiting db script

--- db_script.sql
+++ db_script.sql
@@ -6,3 +6,19 @@ CREATE TABLE applied_companies (
     applied_date TIMESTAMP WITH TIME ZONE NOT NULL,
     UNIQUE (company_name, job_position)
 );
+
+
+CREATE EXTENSION IF NOT EXISTS vector;
+
+CREATE TABLE applied_companies_embeddings (
+    vector_id SERIAL PRIMARY KEY,
+    applied_company_id INT REFERENCES applied_companies(id) ON DELETE CASCADE, -- Foreign key to applied_companies table
+    company_embeddings VECTOR(768) -- SBERT embeddings typically have a dimension of 768
+);
+
+-- Create an index on the embeddings column for cosine similarity
+CREATE INDEX ON applied_companies_embeddings
+USING ivfflat (company_embeddings vector_cosine_ops) WITH (lists = 50);
+
+
+REINDEX INDEX applied_companies_embeddings_embedding_idx;
